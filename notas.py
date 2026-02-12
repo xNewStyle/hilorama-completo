143dcb9ce18092673247c7ed240ae42bd9512a46
xNewStyle <jorgeangel1999@hotmail.com>
2026-02-12 09:29:45 -0600

    Nuevo notas

--- notas.py
+++ notas.py
@@ -311,15 +311,13 @@ def eliminar_nota(id_nota):
 
 def guardar_nota_actualizada(nota_actualizada):
     conn = get_conn()
-    cur = conn.cursor()
 
-    cur.execute("""
+    conn.execute("""
         UPDATE notas
         SET cliente_id=%s,
             cliente_nombre=%s,
             estado=%s,
             total=%s,
-            items=%s,
             envio=%s,
             comprobante=%s
         WHERE id=%s
@@ -328,17 +326,16 @@ def guardar_nota_actualizada(nota_actualizada):
         nota_actualizada["cliente_nombre"],
         nota_actualizada["estado"],
         nota_actualizada["total"],
-        json.dumps(nota_actualizada.get("items", [])),
         json.dumps(nota_actualizada.get("envio", {})),
         nota_actualizada.get("comprobante"),
         nota_actualizada["id"]
     ))
 
     conn.commit()
-    cur.close()
     conn.close()
 
     return True
+
 
 
 def buscar_nota_por_texto(texto):
