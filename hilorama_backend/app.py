372dd4be07831eb0c88d680850ce43d153eb70d9
xNewStyle <jorgeangel1999@hotmail.com>
2026-02-19 23:40:59 -0600

    Agregar 41

--- hilorama_backend/app.py
+++ hilorama_backend/app.py
@@ -699,6 +699,9 @@ app.jinja_env.globals.update(
 
 
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
